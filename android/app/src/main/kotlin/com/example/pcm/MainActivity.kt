4d33f80b617c9b4284d7808121f81965ad0cf984
Anne <annemachinda@gmail.com>
2024-03-25 15:28:16 +0000

    initialization

---
+++ android/app/src/main/kotlin/com/example/pcm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pcm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/styles/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors();
+  static Color primaryDark = const Color(0xff399F8C);
+  static Color primaryLight = const Color(0xff9CDED1);
+  static Color secondaryDark = const Color(0xff51B3F8);
+  static Color secondaryLight = const Color(0xffA9DAFD);
+  static Color plainWhite = const Color(0xffFFFFFF);
+  static Color dark = const Color(0xff1C1B1B);
+  static Color grey = const Color(0xff5F6368);
+  static Color greyBackground = const Color(0xffFCFCFC);
+  
+  // States
+  static Color successLight = const Color(0xffCCFFE6);
+
+  static Color errorLight = const Color(0xFFFFDEDB);
+  static Color errorColor = const Color(0xFFCC3300);
+  static Color infoColor = const Color(0xFF4285F4);
+  static Color warningColor = const Color(0xFFF4B400);
+  static Color successColor = const Color(0xFF0F9D58);
+}
